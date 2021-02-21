0da90e621f8fc914cfb7b8e99634c0e7e54850db
Anderson Dantas <andersondantas81@gmail.com>
2021-02-21 12:00:28 -0300

    Implementação da autenticação com Auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { Provider } from 'next-auth/client';
 import { AppProps } from 'next/app';
 import '../styles/index.css';
-import 'react-datepicker/dist/react-datepicker.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
